f23cd7b6048869c402ff67f543696d855072ec03
Nikolay Orzhekhosvkii <icearrow256@gmail.com>
2020-07-08 20:47:38 +0300

    add winning mechanics & add finish window

--- game.gd
+++ game.gd
@@ -6,4 +6,16 @@ func _ready():
 	stats.connect("won", self, "finish_game")
 
 func finish_game():
+	$CanvasLayer/CenterContainer/WindowDialog.popup()
+	get_tree().paused = true
 	print("You win!")
+
+
+func _on_CanvasLayer_exit_button_pressed():
+	get_tree().quit()
+
+
+func _on_CanvasLayer_restart_button_pressed():
+	get_tree().reload_current_scene()
+	PlayerStats.score = 0
+	get_tree().paused = false
